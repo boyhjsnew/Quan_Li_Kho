7c7f4371155009098413ed4e3c767354d8ff24b0
19dh110745 <19dh110745@gmail.comm>
2023-03-29 21:39:32 +0700

    okhome

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -4,11 +4,6 @@ import React from "react";
 import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import Header from "../../componets/Header";
 import color from "../Home/../../assets/colors/COLORS";
-import img from "../Home/../../assets/images/arrow.png";
-import imgStore from "../Home/../../assets/images/store.png";
-import imgbox from "../Home/../../assets/images/box.png";
-import spacing from "../../assets/dimens/SPACING";
-import Toolbar from "../../componets/ContentHeader";
 import ContentHeader from "../../componets/ContentHeader";
 
 export default function Home() {
@@ -18,7 +13,6 @@ export default function Home() {
         style={{ width: "100%", backgroundColor: color.primary, padding: 10 }}>
         <Header/>
         <ContentHeader/>
-        
       </View>
     </View>
   );
