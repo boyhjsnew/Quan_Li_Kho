dd2bd1ef26c755a833ac8c79bd7670fb3c1d8a94
boyhjsnew <114845328+boyhjsnew@users.noreply.github.com>
2023-04-17 16:42:21 +0700

    qua met

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,15 +1,8 @@
 import React, { useEffect, useState } from "react";
-import {
-  SafeAreaView,
-  ScrollView,
-  StyleSheet,
-  View,
-  Text,
-  Modal,
-  TouchableOpacity,
-} from "react-native";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
 import { Calendar } from "react-native-calendars";
 
+import COLORS from "../assets/colors/COLORS";
 export default function ModalCalendar(props) {
   const [timeStamp, setTimeStamp] = useState(new Date().getDay());
   const [day, setDay] = useState(new Date().getDate());
@@ -17,40 +10,51 @@ export default function ModalCalendar(props) {
   const [year, setYear] = useState(new Date().getFullYear());
   const [selectedDate, setSelectedDate] = useState(null);
   //   const[showCalendar,setShowCalendar] = useState(false)
-  const{setDayNew,setMonthNew,setYearNew} = props
-  
+  const { setDayNew, setMonthNew, setYearNew } = props;
+
   const { handleClose } = props;
   const { setShowCalendar } = props;
   //   alert(showCalendar)
 
   return (
-    <View style={{}}>
+    <View>
       <View
         style={{
           width: "100%",
           height: 90,
-          backgroundColor: "#00bbf2",
+          backgroundColor: COLORS.primary,
           paddingLeft: 30,
           justifyContent: "center",
+          borderTopRightRadius: 30,
+          borderTopLeftRadius: 30,
         }}
       >
-        <Text style={{ color: "#D3D3D3", fontWeight: "700", fontSize: 17 }}>
+        <Text style={{ color: "#ffffff", fontWeight: "700", fontSize: 17 }}>
           {year}
         </Text>
         <View style={{ flexDirection: "row" }}>
-          <Text style={{ color: "white", fontWeight: "700", fontSize: 35 }}>
+          <Text style={{ color: "white", fontWeight: "700", fontSize: 30 }}>
             Th {timeStamp},{" "}
           </Text>
-          <Text style={{ color: "white", fontWeight: "700", fontSize: 35 }}>
+          <Text style={{ color: "white", fontWeight: "700", fontSize: 30 }}>
             {day}
           </Text>
-          <Text style={{ color: "white", fontWeight: "700", fontSize: 35 }}>
+          <Text style={{ color: "white", fontWeight: "700", fontSize: 30 }}>
             {" "}
             thg {month}
           </Text>
         </View>
       </View>
       <Calendar
+        theme={{
+          backgroundColor: "#ffffff",
+          calendarBackground: "#ffffff",
+          textSectionTitleColor: "#b6c1cd",
+          selectedDayBackgroundColor: COLORS.secondary,
+          selectedDayTextColor: "#ffffff",
+          todayTextColor: COLORS.primary,
+          arrowColor: COLORS.primary,
+        }}
         markedDates={{
           [selectedDate]: {
             selected: true,
@@ -65,18 +69,12 @@ export default function ModalCalendar(props) {
           setSelectedDate(day.dateString);
           // setTimeStamp(new Date(day.timestamp).getDay()),
           if (new Date(day.timestamp).getDay() == 0) return setTimeStamp("CN");
-          if (new Date(day.timestamp).getDay() == 1)
-            return setTimeStamp("2");
-          if (new Date(day.timestamp).getDay() == 2)
-            return setTimeStamp("3");
-          if (new Date(day.timestamp).getDay() == 3)
-            return setTimeStamp("4");
-          if (new Date(day.timestamp).getDay() == 4)
-            return setTimeStamp("5");
-          if (new Date(day.timestamp).getDay() == 5)
-            return setTimeStamp("6");
-          if (new Date(day.timestamp).getDay() == 6)
-            return setTimeStamp("7");
+          if (new Date(day.timestamp).getDay() == 1) return setTimeStamp("2");
+          if (new Date(day.timestamp).getDay() == 2) return setTimeStamp("3");
+          if (new Date(day.timestamp).getDay() == 3) return setTimeStamp("4");
+          if (new Date(day.timestamp).getDay() == 4) return setTimeStamp("5");
+          if (new Date(day.timestamp).getDay() == 5) return setTimeStamp("6");
+          if (new Date(day.timestamp).getDay() == 6) return setTimeStamp("7");
         }}
       />
       <View
@@ -93,11 +91,26 @@ export default function ModalCalendar(props) {
             paddingHorizontal: 20,
           }}
         >
-          <TouchableOpacity onPress={()=>setShowCalendar(false)}>
-            <Text style={{ fontSize: 18, paddingHorizontal: 10 }}>Hủy</Text>
+          <TouchableOpacity onPress={() => setShowCalendar(false)}>
+            <Text
+              style={{ fontSize: 15, paddingHorizontal: 10, fontWeight: "600" }}
+            >
+              Hủy
+            </Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={()=>{setDayNew(day),setMonthNew(month),setYearNew(year),setShowCalendar(false)}}>
-            <Text style={{ fontSize: 18, paddingHorizontal: 10 }}>Ok</Text>
+          <TouchableOpacity
+            onPress={() => {
+              setDayNew(day),
+                setMonthNew(month),
+                setYearNew(year),
+                setShowCalendar(false);
+            }}
+          >
+            <Text
+              style={{ fontSize: 15, paddingHorizontal: 10, fontWeight: "600" }}
+            >
+              Xong
+            </Text>
           </TouchableOpacity>
         </View>
       </View>
